0b5227e69eb7d7da28b339efbdb1e040ef2a9428
Faiz-device <abdillahdotfaizz@gmail.com>
2025-02-07 17:55:32 +0700

    slicing

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:basic_widget/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Inter'
+      ),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
